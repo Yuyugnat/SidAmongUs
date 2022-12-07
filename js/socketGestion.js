d9ea4f55cc9b79d738235d6ea2c995cd76a548a8
Guemar Rayan <rayan1guemar@gmail.com>
2022-12-07 10:35:32 +0100

    fix chat

--- js/socketGestion.js
+++ js/socketGestion.js
@@ -41,15 +41,15 @@ socket.on('player-disconnected', id => {
     })
 })
 
-socket.on('chat-message', ({id, message, x, y}) => {
+socket.on('player-chat', ({id, message, x, y}) => {
     const dom = document.createElement('div');
-    const xDiff = mainCharacter.x - x;
-    const yDiff =  mainCharacter.y - y;
+    const xDiff = game.mainCharacter.x - x;
+    const yDiff =  game.mainCharacter.y - y;
     const distance = Math.sqrt(xDiff * xDiff + yDiff * yDiff);
 
     if (distance >= 250) return;
 
-    const sender = listOtherPlayers.find(player => player.id === id);
+    const sender = game.listOtherPlayers.find(player => player.id === id);
     console.log('DISPLAY')
     
     dom.innerHTML = message;
@@ -63,35 +63,3 @@ socket.on('chat-message', ({id, message, x, y}) => {
         dom.remove();
     }, 2000);
 })
-// socket.on("move", (playersList) => {
-//     console.log("coucou", playersList);
-//     playersList.forEach(player => {
-//         console.log(player);
-//         game.listOtherPlayers.push(new OtherCharacter(player.name, player.id))
-//     })
-// })
-
-/*
-switch (event.type) {
-                case 'id':
-                    eventHandler.onId(event.data);
-                    break
-                case 'players-list':
-                    eventHandler.onPlayersList(JSON.parse(event.data));
-                    break
-                case 'new-player':
-                    eventHandler.onNewPlayer(JSON.parse(event.data));
-                    break
-                case 'player-disconnected':
-                    console.log('launch the event', event.type);
-                    console.log(eventHandler.onPlayerDisconnected);
-                    eventHandler.onPlayerDisconnected(parseInt(event.data));
-                    break
-                case 'move':
-                    eventHandler.onMove(JSON.parse(event.data));
-                    break
-                case 'map':
-                    eventHandler.onMap(JSON.parse(event.data));
-                    break
-            }
-*/
